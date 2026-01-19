5389a50d4a5464d68a2cdc082ec14f0620f2342a
kdjh2002 <187482672+kdjh2002@users.noreply.github.com>
2026-01-19 17:48:27 +0900

    프로젝트 세팅

---
+++ Source/Review_Network/Review_Network.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Review_Network.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Review_Network, "Review_Network" );
